8fa5fe9b5d107d0b3e6e4ec75de8bfd610512cd9
Pakhomov Dmitriy Aleksandrovich <d.a.pakhomov@tinkoff.ru>
2020-01-05 18:07:45 +0300

    copy example

--- services/webgateway/app/src/main/scala/com/github/zasport/webgateway/GraphQLGatewayApp.scala
+++ services/webgateway/app/src/main/scala/com/github/zasport/webgateway/GraphQLGatewayApp.scala
@@ -1,15 +1,80 @@
 package com.github.zasport.webgateway
 
-import zio.App
-import zio.console.putStrLn
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import ExampleData.{ sampleCharacters, Character, CharacterArgs, CharactersArgs, Role }
+import caliban.GraphQL.graphQL
+import caliban.schema.Annotations.{ GQLDeprecated, GQLDescription }
+import caliban.schema.GenericSchema
+import caliban.{ AkkaHttpAdapter, RootResolver }
+import zio.clock.Clock
+import zio.console.Console
+import zio.stream.ZStream
+import zio.{ DefaultRuntime, URIO }
 
-object GraphQLGatewayApp extends App {
+import scala.io.StdIn
 
-  def run(args: List[String]) =
-    myAppLogic.map(_ => 0)
+object ExampleApp extends App with GenericSchema[Console with Clock] {
 
-  val myAppLogic =
-    for {
-      _ <- putStrLn("Hello World")
-    } yield ()
+  implicit val system           = ActorSystem()
+  implicit val materializer     = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+  implicit val defaultRuntime   = new DefaultRuntime {}
+
+  implicit val roleSchema           = gen[Role]
+  implicit val characterSchema      = gen[Character]
+  implicit val characterArgsSchema  = gen[CharacterArgs]
+  implicit val charactersArgsSchema = gen[CharactersArgs]
+
+  case class Queries(
+                      @GQLDescription("Return all characters from a given origin")
+                      characters: CharactersArgs => URIO[Console, List[Character]],
+                      @GQLDeprecated("Use `characters`")
+                      character: CharacterArgs => URIO[Console, Option[Character]]
+                    )
+  case class Mutations(deleteCharacter: CharacterArgs => URIO[Console, Boolean])
+  case class Subscriptions(characterDeleted: ZStream[Console, Nothing, String])
+
+  val interpreter = defaultRuntime.unsafeRun(
+    ExampleService
+      .make(sampleCharacters)
+      .map(service => {
+        graphQL(
+          RootResolver(
+            Queries(
+              args => service.getCharacters(args.origin),
+              args => service.findCharacter(args.name)
+            ),
+            Mutations(args => service.deleteCharacter(args.name)),
+            Subscriptions(service.deletedEvents)
+          )
+        ).interpreter
+      })
+  )
+
+  /**
+   * curl -X POST \
+   * http://localhost:8088/api/graphql \
+   * -H 'Host: localhost:8088' \
+   * -H 'Content-Type: application/json' \
+   * -d '{
+   * "query": "query { characters { name }}"
+   * }'
+   */
+  val route =
+    path("api" / "graphql") {
+      AkkaHttpAdapter.makeHttpService(interpreter)
+    } ~ path("graphiql") {
+      getFromResource("graphiql.html")
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8088)
+  println(s"Server online at http://localhost:8088/\nPress RETURN to stop...")
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+
 }
